3f239b3cd47844c75de9cb9e77a55cba5de2c700
Albin Bernhardsson <catscratchesp@gmail.com>
2015-07-03 12:58:13 +0200

    Read input file

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,45 @@ int main(int argc, const char* argv[]) {
     else
         outputName = inputName + ".hzz";
     
-    cout << inputName << " " << outputName;
+    // Read input file.
+    ifstream inFile(inputName.c_str(), ios::binary);
+    char* buffer;
+    int size;
+    if (inFile.is_open()) {
+        size = inFile.tellg();
+        buffer = new char[size];
+        inFile.seekg(0, ios::beg);
+        inFile.read(buffer, size);
+        
+        inFile.close();
+    } else {
+        cout << "Couldn't open " << inputName << "." << endl;
+        return 1;
+    }
     
-    /// @todo: Read and write files
+    // Write output file.
+    ofstream outFile(outputName.c_str(), ios::trunc);
+    if (outFile.is_open()) {
+        writeHeader(outFile);
+        writeData(outFile);
+        writeFooter(outFile);
+        
+        outFile.close();
+    } else {
+        cout << "Couldn't open " << outputName << " for writing." << endl;
+    }
     
     return 0;
+}
+
+void writeHeader(ofstream& outFile) {
+    /// @todo: Write header
+}
+
+void writeData(ofstream& outFile) {
+    /// @todo: Write data
+}
+
+void writeFooter(ofstream& outFile) {
+    /// @todo: Write footer
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,6 +1,8 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#include <fstream>
+
 /// Main function.
 /**
  * @param argc Argument count.
@@ -8,5 +10,23 @@
  * @return 0 on success
  */
 int main(int argc, const char* argv[]);
+
+/// Write header.
+/**
+ * @param outFile File stream to write to.
+ */
+void writeHeader(std::ofstream& outFile);
+
+/// Write data.
+/**
+ * @param outFile File stream to write to.
+ */
+void writeData(std::ofstream& outFile);
+
+/// Write footer.
+/**
+ * @param outFile File stream to write to.
+ */
+void writeFooter(std::ofstream& outFile);
 
 #endif
