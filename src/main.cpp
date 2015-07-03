e3f256d403af38253d3a5615d141c81c03f3372b
Albin Bernhardsson <catscratchesp@gmail.com>
2015-07-03 12:44:28 +0200

    Get input and output filenames

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,30 @@
 
 #include <cstring>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, const char* argv[]) {
     // Print help
-    if (argc == 0 || strcmp(argv[0], "--help")) {
+    if (argc == 1 || strcmp(argv[0], "--help") == 0) {
         cout << "Headerize is a tool to convert resource files to headers for inclusion in C/C++ projects." << endl
              << "Usage:" << endl
              << "Headerize input output" << endl;
         return 0;
     }
     
-    /// @todo: Actual usage
+    // Get input and output names.
+    string inputName = argv[1];
+    string outputName;
+    if (argc > 2)
+        outputName = argv[2];
+    else
+        outputName = inputName + ".hzz";
+    
+    cout << inputName << " " << outputName;
+    
+    /// @todo: Read and write files
     
     return 0;
 }
